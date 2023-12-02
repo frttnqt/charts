512277546f2f782a5531fa72a929a1550ef33caa
Artsiom Shylau <a.shilov.by@gmail.com>
2023-12-02 22:45:00 +0800

    added color scheme support

--- src/app/common/chart-view/chart-view.component.ts
+++ src/app/common/chart-view/chart-view.component.ts
@@ -1,13 +1,25 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { ChartConfigData, ChartData, ChartMeter } from '@app/models/chart-data';
 import { BarChartModule, LineChartModule, PieChartModule } from '@swimlane/ngx-charts';
-import { NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
+import { NgForOf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import { isDataWithinTimeInterval } from '@app/utils/time.helper';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chart-view',
   standalone: true,
-  imports: [BarChartModule, NgSwitchCase, NgSwitch, LineChartModule, PieChartModule, NgSwitchDefault],
+  imports: [
+    BarChartModule,
+    NgSwitchCase,
+    NgSwitch,
+    LineChartModule,
+    PieChartModule,
+    NgSwitchDefault,
+    NzSelectModule,
+    NgForOf,
+    FormsModule,
+  ],
   templateUrl: './chart-view.component.html',
   styleUrl: './chart-view.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -16,6 +28,10 @@ export class ChartViewComponent {
   @Input() public dateRange: Date[];
   @Input() public chart: ChartData;
   @Input() public chartConfig: ChartConfigData;
+
+  public availableColorSchemes = ['vivid', 'natural', 'cool', 'fire'];
+
+  public colorScheme = 'vivid';
 
   public get initialData(): ChartMeter[] {
     return this.chart.data.filter(sensor => isDataWithinTimeInterval(sensor, this.dateRange));
